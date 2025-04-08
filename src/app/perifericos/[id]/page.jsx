5b5ad297337de5856742ff0042b0fadac66c8b97
GiovanniBBenedetti <senaigiovannibenedetti@gmail.com>
2025-04-08 10:44:23 -0300

    alteração no fetch da api

--- src/app/perifericos/[id]/page.jsx
+++ src/app/perifericos/[id]/page.jsx
@@ -5,7 +5,7 @@ export default async function DetailsCard({ params }) {
     const { id } = await params;
 
  
-    const response = await fetch(`http://localhost:3333/cards1/${id}`);
+    const response = await fetch(`http://localhost:3333/cards/${id}`);
 
     if (!response.ok) {
         redirect("/not-found.jsx");
